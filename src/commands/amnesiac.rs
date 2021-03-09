f409e7f393469e620783975d9adfb405b856695b
TheUserCreated <boswinle001@gmail.com>
2021-03-09 15:31:20 -0600

    updates some dependencies. adds the `autodelete check` command

--- src/commands/amnesiac.rs
+++ src/commands/amnesiac.rs
@@ -6,29 +6,60 @@ use serenity::{
 };
 use serenity::static_assertions::_core::str::FromStr;
 use tokio::time::{Duration, sleep};
-use tracing::{error, info};
+use tracing::{info};
 
 use crate::structures::data::{ChannelMap, ConnectionPool};
 
 #[command]
 #[required_permissions("MANAGE_GUILD")]
 #[only_in(guilds)]
-#[sub_commands(set)]
+#[sub_commands(set, check, remove)]
 async fn autodelete(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let channel_id = args.current().expect("no role found");
-    let channel_id = ChannelId::from_str(channel_id).expect("couldn't unpack roleid from argument");
-    args.advance();
-    let time = args.current().expect("time not found");
-    let time = i64::from_str(time).expect("couldnt get time");
-    info!("role is {} and time is {}", channel_id, time);
+    msg.reply(ctx,"Not enough arguments! Run `help autodelete` to see how to use this command.").await?;
+    //let channel_id = args.current().expect("no role found");
+    //let channel_id = ChannelId::from_str(channel_id).expect("couldn't unpack roleid from argument");
+    //args.advance();
+    //let time = args.current().expect("time not found");
+    //let time = i64::from_str(time).expect("couldnt get time");
 
-
+    Ok(())
+}
+#[command]
+#[only_in(guilds)]
+#[aliases("delete", "unset")]
+async fn remove(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult{
     Ok(())
 }
 
 #[command]
+#[only_in(guilds)]
+async fn check(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let channelmap = {
+        let data = ctx.data.read().await;
+        let amnesiacs = data.get::<ChannelMap>().cloned().unwrap();
+        amnesiacs
+
+    };
+
+    let channel_id = args.current().expect("no channel found");
+    let channel_id = ChannelId::from_str(channel_id).expect("couldn't unpack channelid from argument");
+    let result = channelmap.get(&channel_id);
+    if let Some(result) = result {
+        let minutes = result.value();
+        let response = format!("The channel {} has auto-delete enabled, with a timer of {} minute(s).", channel_id.name(ctx).await.expect("channel doesnt have a name?"), minutes);
+        msg.reply(ctx,response).await?;
+
+    } else {
+        msg.reply(ctx, "That channel doesn't seem to have auto-delete enabled").await?;
+    }
+    Ok(())
+}
+
+
+#[command]
 #[required_permissions("MANAGE_GUILD")]
 #[only_in(guilds)]
+#[aliases("add")]
 async fn set(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let channel_id = args.current().expect("no role found");
     let channel_id = ChannelId::from_str(channel_id).expect("couldn't unpack roleid from argument");
